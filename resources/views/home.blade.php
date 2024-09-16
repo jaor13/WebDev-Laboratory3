766d39e7e665041f6edb5a1b44aad417c75e7fef
jaor13 <jaor2022-6131-98317@bicol-u.edu.ph>
2024-09-16 23:42:42 +0800

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,6 @@
         <a href="/shop" class="btn btn-shop-now btn-lg">Shop Now</a>
     </div>
 </div>
-
 @endsection
 
 @section('content')
@@ -19,43 +18,31 @@
     <div class="row mt-4">
         <div class="col-md-4">
             <div class="card">
-                <img src="images/a.jpg" class="card-img-top" alt="Bouquet 1">
+                <img src="{{ asset('asset/images/a.jpg') }}" class="card-img-top" alt="Bouquet 1">
                 <div class="card-body">
                     <h5 class="card-title">Bouquet 1</h5>
                     <p class="card-text">A stunning mix of artificial flowers and chocolates perfect for any occasion.</p>
                 </div>
             </div>
         </div>
-
-    <div class="container mt-5">
-        <h2 class="text-center">Best Sellers</h2>
-        <div class="row mt-4">
-            <div class="col-md-4">
-                <div class="card">
-                <img src="{{ asset('asset/images/a.jpg') }}" class="card-img-top" alt="Bouquet 1">
-                    <div class="card-body">
-                        <h5 class="card-title">Bouquet 1</h5>
-                        <p class="card-text">A stunning mix of artificial flowers and chocolates perfect for any occasion.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
+        <div class="col-md-4">
+            <div class="card">
                 <img src="{{ asset('asset/images/b.jpg') }}" class="card-img-top" alt="Bouquet 2">
-                    <div class="card-body">
-                        <h5 class="card-title">Bouquet 2</h5>
-                        <p class="card-text">Brighten up someone's day with this vibrant arrangement.</p>
-                    </div>
+                <div class="card-body">
+                    <h5 class="card-title">Bouquet 2</h5>
+                    <p class="card-text">Brighten up someone's day with this vibrant arrangement.</p>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card">
+        </div>
+        <div class="col-md-4">
+            <div class="card">
                 <img src="{{ asset('asset/images/c.jpg') }}" class="card-img-top" alt="Bouquet 3">
-                    <div class="card-body">
-                        <h5 class="card-title">Bouquet 3</h5>
-                        <p class="card-text">A classic bouquet featuring timeless floral favorites.</p>
-                    </div>
+                <div class="card-body">
+                    <h5 class="card-title">Bouquet 3</h5>
+                    <p class="card-text">A classic bouquet featuring timeless floral favorites.</p>
                 </div>
             </div>
         </div>
     </div>
+</div>
+@endsection
